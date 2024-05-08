c7553cf366024c4dc3fd9a8381e344605a832134
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-05-08 19:27:55 -0300

    adicao de arquivo css para o footer

---
+++ backend/login.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("template/header.php");
+?>
+    <h1>PLA</h1>
+<?php
+    require_once("template/footer.php");
+?>
---
+++ backend/sobre.php
@@ -0,0 +1,8 @@
+<?php
+    require_once("template/header.php");
+?>
+    <h1>Página de Estudos Senac 2024
+    </h1>
+<?php
+    require_once("template/footer.php");
+?>
