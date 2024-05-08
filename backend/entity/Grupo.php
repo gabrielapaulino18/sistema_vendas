0d99d2836bd26aa66fa737a2ca63e2e7be816257
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-05-08 19:28:30 -0300

    Classe GrupoUsuario

---
+++ backend/entity/Grupo.php
@@ -0,0 +1,55 @@
+<?php
+
+class Grupo {
+
+    private $id;
+    private $nome;
+    private $descricao;
+    private $dataCriacao;
+    private $dataAtualizacao;
+    private $usuarioAtualizacao;
+    private $ativo;
+
+    public function __construct($id, $nome, $descricao, $dataCriacao, $dataAtualizacao, $usuarioAtualizacao, $ativo) {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->descricao = $descricao;
+        $this->dataCriacao = $dataCriacao;
+        $this->dataAtualizacao = $dataAtualizacao;
+        $this->usuarioAtualizacao = $usuarioAtualizacao;
+        $this->ativo = $ativo;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    public function getDataCriacao() {
+        return $this->dataCriacao;
+    }
+
+    public function getDataAtualizacao() {
+        return $this->dataAtualizacao;
+    }
+
+    public function getUsuarioAtualizacao() {
+        return $this->usuarioAtualizacao;
+    }
+
+    public function getDataAtivo() {
+        return $this->ativo;
+    }
+
+}
+
+
+
+?>
